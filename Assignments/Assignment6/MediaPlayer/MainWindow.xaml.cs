2f8eb32d6a9da46e178e928c8c8dd209ac882d9f
Anatoli Dimitrov Dvoyachki <253651@VIAUC.DK>
2019-03-23 20:00:08 +0100

    initial assignment 6 work

---
+++ Assignments/Assignment6/MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DNP2.Assignments.MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
