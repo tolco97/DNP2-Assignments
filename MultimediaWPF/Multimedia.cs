f28366bf39bd45af24959e4516ab07ad5761b06f
Anatoli Dimitrov Dvoyachki <253651@VIAUC.DK>
2019-03-23 19:36:58 +0100

    properties in Multimedia

--- MultimediaWPF/Multimedia.cs
+++ MultimediaWPF/Multimedia.cs
@@ -6,34 +6,13 @@ namespace DNP2.Assignment5.MultimediaWPF
 {
     public class Multimedia : INotifyPropertyChanged
     {
-        private string _artist;
-        private string _genre;
-        private string _title;
-        private MediaType _type;
+        public string Artist { get; set; }
 
-        public string Artist
-        {
-            get => _artist;
-            set => _artist = value;
-        }
+        public string Genre { get; set; }
 
-        public string Genre
-        {
-            get => _genre;
-            set => _genre = value;
-        }
+        public string Title { get; set; }
 
-        public string Title
-        {
-            get => _title;
-            set => _title = value;
-        }
-
-        public MediaType Type
-        {
-            get => _type;
-            set => _type = value;
-        }
+        public MediaType Type { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
