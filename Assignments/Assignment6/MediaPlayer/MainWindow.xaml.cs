ab470e95eef9605dcd229a672992975bfd072765
Anatoli Dimitrov Dvoyachki <253651@VIAUC.DK>
2019-03-24 14:49:38 +0100

    Assignment 6 almost finished

--- Assignments/Assignment6/MediaPlayer/MainWindow.xaml.cs
+++ Assignments/Assignment6/MediaPlayer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows;
+using System.IO;
+using System.Windows.Threading;
 using Microsoft.Win32;
 
 namespace DNP2.Assignments.MediaPlayer
@@ -9,17 +11,44 @@ namespace DNP2.Assignments.MediaPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            _timer.Tick += OnTimerTick;
+            _timer.Start();
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            if (MediaViewer.Source != null && MediaViewer.NaturalDuration.HasTimeSpan)
+            {
+                ProgressBar.Minimum = 0;
+                ProgressBar.Maximum = MediaViewer.NaturalDuration.TimeSpan.TotalSeconds;
+                ProgressBar.Value = MediaViewer.Position.TotalSeconds;
+            }
         }
 
         private void OpenButton_Click(object sender, RoutedEventArgs e)
         {
+            // Initialize Media element with media file
             var openFileDialog = new OpenFileDialog();
-            openFileDialog.ShowDialog();
-            MediaViewer.Source = new Uri(openFileDialog.FileName);
-            MediaViewer.Play();
+            bool? success = openFileDialog.ShowDialog();
+
+            if (success.HasValue && success.Value)
+            {
+                MediaViewer.Source = new Uri(openFileDialog.FileName);
+
+                // Update progress label
+                FilePlayingLabel.Content = $"Playing: {Path.GetFileName(openFileDialog.FileName)}";
+
+                // Update progress bar
+                ProgressBar.Value = 0;
+                ProgressBar.Minimum = 0;
+                ProgressBar.Maximum = MediaViewer.Position.TotalSeconds;
+            }
         }
 
         private void PlayButton_Click(object sender, RoutedEventArgs e)
@@ -40,6 +69,11 @@ namespace DNP2.Assignments.MediaPlayer
         private void MediaPlayer_OnMediaFailed(object sender, ExceptionRoutedEventArgs e)
         {
             MessageBox.Show("Media loading unsuccessful. " + e.ErrorException.Message);
+        }
+
+        private void ProgressBar_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ProgressLabel.Content = TimeSpan.FromSeconds(ProgressBar.Value).ToString(@"hh\:mm\:ss");
         }
     }
 }
