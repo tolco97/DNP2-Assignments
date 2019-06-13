0629ac637cd978f78125aac567c5fa2f4ad7e597
Anatoli Dvoyachki <tolco97@abv.bg>
2019-06-13 10:21:24 +0200

    a

--- Assignments/Common/ExtensionMethods.cs
+++ Assignments/Common/ExtensionMethods.cs
@@ -6,7 +6,7 @@ namespace DNP2.Helpers.Common
     public static class ExtensionMethods
     {
         /// <summary>
-        /// Prints all elements of the enumerable on a new line
+        /// Prints all elements of the enumerable (each element on a new line)
         /// </summary>
         /// <param name="collection">the enumerable</param>
         public static void PrintAll<T>(this IEnumerable<T> collection)
