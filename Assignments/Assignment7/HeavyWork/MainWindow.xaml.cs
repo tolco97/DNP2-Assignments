a3ede39740cb160bb097473c0914d95c852ae3e2
Anatoli Dvoyachki <tolco97@abv.bg>
2019-06-15 20:15:37 +0200

    Assignment 7 fixed

--- Assignments/Assignment7/HeavyWork/MainWindow.xaml.cs
+++ Assignments/Assignment7/HeavyWork/MainWindow.xaml.cs
@@ -39,47 +39,15 @@ namespace DNP2.Assignment7.HeavyWork
                 _heavyTasks.Clear();
             }
 
-            // 1) Start 3 tasks in parallel
-            await Task.Run(() =>
-            {
-                Parallel.Invoke
-                (
-                    async () =>
-                    {
-                        Task t1 = HeavyWorkAsync();
+            OutputTextBox.Text += "Task 1 started\n"; // 2) Indicate that each task has started
+            _heavyTasks.Add(HeavyWorkAsync());
 
-                        _heavyTasks.Add(t1);
-                        Dispatcher.Invoke(() => OutputTextBox.Text += "Task 1 started\n"); // 2) Indicate that each task has started
+            OutputTextBox.Text += "Task 2 started\n"; // 2) Indicate that each task has started
+            _heavyTasks.Add(HeavyWorkAsync());
 
-                        await t1;
-                    },
-                    async () =>
-                    {
-                        Task t2 = HeavyWorkAsync();
-
-                        _heavyTasks.Add(t2);
-                        Dispatcher.Invoke(() => OutputTextBox.Text += "Task 2 started\n"); // 2) Indicate that each task has started
-
-                        await t2;
-                    },
-                    async () =>
-                    {
-                        Task t3 = HeavyWorkAsync();
-
-                        _heavyTasks.Add(t3);
-                        Dispatcher.Invoke(() => OutputTextBox.Text += "Task 3 started\n"); // 2) Indicate that each task has started
-
-                        await t3;
-                    }
-                );
-            });
-            
-            // 3) Update label when all 3 tasks are completed
-            await Task.WhenAll(_heavyTasks).ContinueWith(_ =>
-            {
-                Dispatcher.Invoke(() => OutputTextBox.Text += $"All {_heavyTasks.Count} tasks completed\n"); 
-            });
-
+            OutputTextBox.Text += "Task 3 started\n"; // 2) Indicate that each task has started
+            _heavyTasks.Add(HeavyWorkAsync());
+        
             // 4) If not all 3 tasks are completed within 8 seconds indicate that there is still work to be done
             await Task.Delay(8000).ContinueWith(_ =>
             {
@@ -88,6 +56,12 @@ namespace DNP2.Assignment7.HeavyWork
                 {
                     Dispatcher.Invoke(() => OutputTextBox.Text += "Still work to do...\n");
                 }
+            });
+
+            // 3) Update label when all 3 tasks are completed
+            await Task.WhenAll(_heavyTasks).ContinueWith(_ =>
+            {
+                Dispatcher.Invoke(() => OutputTextBox.Text += $"All {_heavyTasks.Count} tasks completed\n"); 
             });
         }
 
